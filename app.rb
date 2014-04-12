7d82c6670ae2077556b55a108025a269af7b1886
曾令发 <zlfera@gmail.com>
2014-04-12 23:24:08 +0000

    0

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ module App
   class Gougou < Sinatra::Base
     enable :logging
     Dir.glob('./controllers/*.rb').each {|file| require file}
-    TOKEN =igougougou'
+    TOKEN = 'igougougou'
     timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
     codes = [TOKEN, timestamp, nonce].sort.join('')
     halt(401) unless Digest::SHA1.hexdigest(codes) == params[:signature]
