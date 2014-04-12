efbdb6b47a991a01263bf92cbffd10effb6912ed
曾令发 <zlfera@gmail.com>
2014-04-12 23:08:53 +0000

    0

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 #coding:utf-8
 module App
-  class Weixin < Sinatra::Base
+  class Gougou < Sinatra::Base
     enable :logging
     Dir.glob('./controllers/*.rb').each {|file| require file}
-    TOKEN = '903953_k'
+    TOKEN =igougougou'
     timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
     codes = [TOKEN, timestamp, nonce].sort.join('')
     halt(401, 'wrong') unless Digest::SHA1.hexdigest(codes) == params[:signature]
--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 require "bundler"
 Bundler.require
 require './app'
-run App::Weixin
+run App::Gougou
