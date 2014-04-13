15bf9ff11ef78c14637ab86bc77ff61a25e8c73b
曾令发 <zlfera@gmail.com>
2014-04-13 11:21:02 +0000

    0

--- controllers/index.rb
+++ controllers/index.rb
@@ -1,13 +1,13 @@
 module App
   class Gougou < Sinatra::Base
-  get '/index' do
-    params[:echostr]
-  end
-
-  post '/index' do
-    if params[:xml][:MsgType] == "text"
-      render "echo", :formats => :xml
+    get '/index' do
+      params[:echostr]
     end
 
+    post '/index' do
+      if params[:xml][:MsgType] == "text"
+        render "echo", :formats => :xml
+      end
+    end
   end
 end
