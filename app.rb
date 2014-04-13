9b01dbaec975af805f12b3133be05d0d55a85f3e
曾令发 <zlfera@gmail.com>
2014-04-13 05:23:30 +0000

    0

--- app.rb
+++ app.rb
@@ -4,9 +4,10 @@ module App
     enable :logging
     Dir.glob('./controllers/*.rb').each {|file| require file}
     TOKEN = 'igougougou'
-    timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
-    codes = [TOKEN, timestamp, nonce].sort.join('')
-    halt(401) unless Digest::SHA1.hexdigest(codes) == params[:signature]
-
+    before do
+      timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
+      codes = [TOKEN, timestamp, nonce].sort.join('')
+      halt(401,'wrong') unless Digest::SHA1.hexdigest(codes) == params[:signature]
+    end
   end
 end
