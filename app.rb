340cd1419df678f7fff74edee042564051cd08d6
曾令发 <zlfera@gmail.com>
2014-04-16 09:43:45 +0000

    0

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ module App
       require 'digest/sha1'
       timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
       codes = [TOKEN, timestamp, nonce].sort.join()
-      status 401 unless Digest::SHA1.hexdigest(codes) == params[:signature]
+      halt(401, 'go out') unless Digest::SHA1.hexdigest(codes) == params[:signature]
     end
     
 
