81256be131cb4e5336b3584a24c5c69e2c4e71e0
曾令发 <zlfera@gmail.com>
2014-04-16 12:29:18 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -1,18 +1,16 @@
 module App
   class Gougou < Sinatra::Base
-   # gett '/weixin_api' do
-    #  params[:echostr]
-    #end
-    
-    #post '/weixin_api' do
-      
-     # if params[:xml][:MsgType] == 'text'
-      #  slim :'weixin_api/reply'
-      #end
-    #end
     get '/weixin_api' do
-      slim :'/weixin_api/reply'
+      params[:echostr]
     end
+    
+    post '/weixin_api' do
+      
+      if params[:xml][:MsgType] == 'text'
+        slim :'weixin_api/reply'
+      end
+    end
+
     
   end
 end
