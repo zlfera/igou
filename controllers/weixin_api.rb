6d99a3509d56c207993210ea2a708801c8082d72
曾令发 <zlfera@gmail.com>
2014-04-16 11:59:57 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -4,13 +4,13 @@ module App
     #  params[:echostr]
     #end
     
-    post '/weixin_api' do
+    #post '/weixin_api' do
       
-      if params[:xml][:MsgType] == 'text'
-        slim :'weixin_api/reply'
-      end
-    end
-    get '/weixin_api' do
+     # if params[:xml][:MsgType] == 'text'
+      #  slim :'weixin_api/reply'
+      #end
+    #end
+    get '/weixin_api/reply' do
       slim :'weixin_api/reply'
     end
     
