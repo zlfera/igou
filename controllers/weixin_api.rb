da9623c56e9215524c07744d952cf8e9c0acc014
曾令发 <zlfera@gmail.com>
2014-04-16 09:52:12 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -7,7 +7,7 @@ module App
     post '/weixin_api' do
       
       if params[:xml][:MsgType] == 'text'
-        #slim :reply, :format :xml
+        slim :reply, :format :xml
       end
     end
     
