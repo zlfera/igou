747419e34438ac9c3fc429024285f16415be5b14
曾令发 <zlfera@gmail.com>
2014-04-17 13:34:58 +0000

    0

--- app.rb
+++ app.rb
@@ -11,12 +11,12 @@ module App
     end
     Dir.glob('./controllers/*.rb').each {|file| require file}
     TOKEN = 'igougougou'
-    #before do
-     # require 'digest/sha1'
-      #timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
-      #codes = [TOKEN, timestamp, nonce].sort.join()
-      #status 200 unless Digest::SHA1.hexdigest(codes) == params[:signature]
-    #end
+    before do
+      require 'digest/sha1'
+      timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
+      codes = [TOKEN, timestamp, nonce].sort.join()
+      status 200 unless Digest::SHA1.hexdigest(codes) == params[:signature]
+    end
     
 
     
