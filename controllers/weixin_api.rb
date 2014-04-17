42a1e0c7e439128d7f2bfcc1a66cd35f35a7c408
曾令发 <zlfera@gmail.com>
2014-04-17 04:07:08 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -6,9 +6,9 @@ module App
     
     post '/weixin_api' do
       content_type :xml
-      if params[:xml][:MsgType] == 'text'
+      #if params[:xml][:MsgType] == 'text'
         slim :'weixin_api/reply'
-      end
+      #end
     end
     get '/' do
       'zeng'
