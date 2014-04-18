f6213446a27fd5e8acce1a1c02b0b28f3335f70e
曾令发 <zlfera@gmail.com>
2014-04-18 12:15:32 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -7,7 +7,7 @@ module App
     post '/weixin_api', provides: 'xml' do
       content_type :xml, charset: 'utf-8'
       request.body.rewind
-      if params[:xml][:MsgType] == 'text'
+      #if params[:xml][:MsgType] == 'text'
         cd = "<xml>
                 <ToUserName><![CDATA[ #{params[:xml][:FromUserName]} ]]></ToUserName>
                 <FromUserName><![CDATA[ #{params[:xml][:ToUserName]} ]]></FromUserName>
@@ -17,10 +17,9 @@ module App
                 <FuncFlag>0</FuncFlag>
               </xml>"
         
-      end
+      #end
     end
     get '/' do
-      content_type 'text/xml'
       slim :'home/index'
     end
     
