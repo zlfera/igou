f4a6bd66addc97546dc6d21cd86848e54694b5e4
曾令发 <zlfera@gmail.com>
2014-04-18 12:13:00 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -9,18 +9,18 @@ module App
       request.body.rewind
       if params[:xml][:MsgType] == 'text'
         cd = "<xml>
-    <ToUserName><![CDATA[ params[:xml][:FromUserName] ]]></ToUserName>
-    <FromUserName><![CDATA[ params[:xml][:ToUserName] ]]></FromUserName>
-    <CreateTime> Time.now.to_i </CreateTime>
-    <MsgType><![CDATA[text]]></MsgType>
-    <Content><![CDATA[re: params[:xml][:Content] ]]></Content>
-    <FuncFlag>0</FuncFlag>
-</xml>
-"
+                <ToUserName><![CDATA[ #{params[:xml][:FromUserName]} ]]></ToUserName>
+                <FromUserName><![CDATA[ #{params[:xml][:ToUserName]} ]]></FromUserName>
+                <CreateTime> #{Time.now.to_i} </CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[re: #{params[:xml][:Content]} ]]></Content>
+                <FuncFlag>0</FuncFlag>
+              </xml>"
         
       end
     end
     get '/' do
+      content_type 'text/xml'
       slim :'home/index'
     end
     
