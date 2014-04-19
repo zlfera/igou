14942cda78e9643028e50fae187ca9c55dac0796
曾令发 <zlfera@gmail.com>
2014-04-19 09:16:05 +0000

    0

--- app.rb
+++ app.rb
@@ -2,6 +2,20 @@
 module App
   
   class Gougou < Sinatra::Base
+    
+    helpers do
+      def check_token
+        require 'digest/sha1'
+        timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
+        codes = [TOKEN, timestamp, nonce].sort.join()
+        if Digest::SHA1.hexdigest(codes) == params[:signature]
+          true
+        else
+          'fff'
+        end
+          
+      end
+    end
 
     configure do
     enable :logging
@@ -12,12 +26,12 @@ module App
     end
     Dir.glob('./controllers/*.rb').each {|file| require file}
     TOKEN = 'igougougou'
-    before do
-      require 'digest/sha1'
-      timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
-      codes = [TOKEN, timestamp, nonce].sort.join()
-      status 200 unless Digest::SHA1.hexdigest(codes) == params[:signature]
-    end
+    #before do
+      #require 'digest/sha1'
+      #timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
+      #codes = [TOKEN, timestamp, nonce].sort.join()
+      #status 200 unless Digest::SHA1.hexdigest(codes) == params[:signature]
+    #end
     
 
     
