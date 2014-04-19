ef3e04bc1c991cee0260c92bbbb716a172ac0e4c
曾令发 <zlfera@gmail.com>
2014-04-19 09:07:08 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -14,7 +14,7 @@ module App
 
     end
     
-    post '/weixin_apis', provides: 'xml' do
+    post '/weixin_api', provides: 'xml' do
       #content_type :xml, charset: 'utf-8'
       #request.body.rewind
       content_type 'text/xml'
