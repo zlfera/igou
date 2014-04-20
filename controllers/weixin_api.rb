a96aaca8b309ef3b751ce5ebbcaa2d9912a3ca4f
曾令发 <zlfera@gmail.com>
2014-04-20 07:08:35 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -6,7 +6,7 @@ module App
     
     post '/weixin_api', provides: 'xml' do
       content_type :xml, charset: 'utf-8'
-      if params[:xml][:MsgType] == 'text'
+      #if params[:xml][:MsgType] == 'text'
         
         root = Nokogiri::XML(request.body.read).root
         @receiver = root.xpath("ToUserName").children.text
@@ -15,11 +15,11 @@ module App
         @keyword = root.xpath("Content").children.text
         @message_type = root.xpath("MsgType").children.text
         @message_id = root.xpath("MsgId").text.to_i
-        #nokogiri :'home/reply'
+        nokogiri :'home/reply'
 
         
-        slim :'home/reply'
-      end
+        #slim :'home/reply'
+      #end
     end
     get '/' do
       slim :'home/index'
