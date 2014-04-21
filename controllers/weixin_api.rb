db8ba15e1a1e3aef0e3f0bbdd10975b15509394c
曾令发 <zlfera@gmail.com>
2014-04-21 10:45:19 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -7,16 +7,16 @@ module App
     post '/weixin_api' do
       content_type :xml, charset: 'utf-8'
       
-        @message_type = 'text'
+        #@message_type = 'text'
         root = Nokogiri::XML(request.body.read).root
-      if @message_type == root.xpath('MsgType').children.text
+      if @message_type = root.xpath('MsgType').children.text
         @receiver = root.xpath("ToUserName").children.text
         @sender = root.xpath("FromUserName").children.text
         @send_time = Time.at(root.xpath("CreateTime").text.to_i)
         @keyword = root.xpath("Content").children.text
-        @message_type = root.xpath("MsgType").children.text
+        #@message_type = root.xpath("MsgType").children.text
         @message_id = root.xpath("MsgId").text.to_i
-        nokogiri :'weixin_api/reply'
+        nokogiri :"weixin/#{@message_type}"
 
         
         #slim :'weixin_api/reply'
