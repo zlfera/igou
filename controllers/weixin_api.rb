0abdc01c9ec92ff1f327074fd09aef9bdd866955
曾令发 <zlfera@gmail.com>
2014-04-21 23:20:36 +0000

    0

--- controllers/weixin_api.rb
+++ controllers/weixin_api.rb
@@ -9,18 +9,17 @@ module App
       
       
       root = Nokogiri::XML(request.body.read).root
-      
-      if @message_type = root.css('MsgType').children.text
-        @receiver = root.css("ToUserName").children.text
-        @sender = root.css("FromUserName").children.text
-        @send_time = Time.at(root.css("CreateTime").text.to_i)
-        @keyword = root.css("Content").children.text
-        @message_id = root.css("MsgId").text.to_i
+      @message_type = root.css('MsgType').children.text
         
-        nokogiri :"weixin_api/#{@message_type.to_s}"
+      @receiver = root.css("ToUserName").children.text
+      @sender = root.css("FromUserName").children.text
+      @send_time = Time.at(root.css("CreateTime").text.to_i)
+      @keyword = root.css("Content").children.text
+      @message_id = root.css("MsgId").text.to_i
+        
+      nokogiri :"weixin_api/#{@message_type.to_s}"
 
         
-      end
     end
     get '/' do
       slim :'home/index'
