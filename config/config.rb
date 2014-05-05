e8003cc533619533cded4e873e6aa275cfdebda7
曾令发 <zlfera@gmail.com>
2014-05-05 23:11:46 +0000

    0

--- config/config.rb
+++ config/config.rb
@@ -1,6 +1,9 @@
 env = ENV['RACK_ENV'] || :development
 Mongoid.load!('./config/mongoid.yml', env)
 configure :production do
+  set :public_folder, File.expand_path('../../public', __FILE__) 
+  set :views, File.expand_path('../../views', __FILE__)
+  set :slim, layout_options: { views: 'views/layouts' }
   require 'newrelic_rpm'
   enable :logging
   GC::Profiler.enable
