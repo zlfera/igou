26b7ad5371f2c073c06d44d2aad45f5c633b4e30
曾令发 <zlfera@gmail.com>
2014-05-11 10:25:35 +0000

    0

--- config/config.rb
+++ config/config.rb
@@ -2,7 +2,7 @@ Dir.glob('./{helpers,models,controllers}/*.rb').each { |file| require file }
 env = ENV['RACK_ENV'] || :development
 Mongoid.load!('./config/mongoid.yml', env)
 configure :production do
-  require 'newrelic_rpm'
+#  require 'newrelic_rpm'
   enable :logging
   GC::Profiler.enable
 end
