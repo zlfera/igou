85c4754d9f7631a650f3d62b1a586b4e5ee2f974
曾令发 <zlfera@gmail.com>
2014-05-11 01:02:19 +0000

    0

--- config/config.rb
+++
@@ -1,8 +0,0 @@
-env = ENV['RACK_ENV'] || :development
-Mongoid.load!('./config/mongoid.yml', env)
-configure :production do
-  require 'newrelic_rpm'
-  enable :logging
-  GC::Profiler.enable
-
-end
