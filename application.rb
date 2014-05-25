c1e41826486839fa2126dcccc35cd8bd00c09e49
曾令发 <zlfera@gmail.com>
2014-05-25 06:57:41 +0000

    0

--- application.rb
+++ application.rb
@@ -5,6 +5,8 @@
 end
 
 libdir = File.expand_path('../../lib', __FILE__)
+a = File.expand_path('./controllers', __FILE__)
+$LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 # env = ENV['RACK_ENV'] || :development
 Mongoid.load!('./config/mongoid.yml', ENV['RACK_ENV'])
