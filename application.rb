7f7b267cfad3e9ca1e8973f08560a00fe58f39c6
曾令发 <zlfera@gmail.com>
2014-05-25 07:05:31 +0000

    0

--- application.rb
+++ application.rb
@@ -1,8 +1,5 @@
 libdir = File.expand_path('../../lib', __FILE__)
-a = File.expand_path('./controllers', __FILE__)
-$LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-# env = ENV['RACK_ENV'] || :development
 Mongoid.load!('./config/mongoid.yml', ENV['RACK_ENV'])
 configure :production do
   require 'newrelic_rpm'
@@ -11,6 +8,8 @@ configure :production do
 end
 
 %w(helpers models controllers).each do |dir|
+  a = File.expand_path("./#{dir}", __FILE__)
+  $LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
   Dir.glob(File.expand_path("../#{dir}", __FILE__) + '/*.rb').each do |file|
     require file
   end
