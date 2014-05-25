95c65257cb7fce3474b25bec11d6ca4c449cc138
曾令发 <zlfera@gmail.com>
2014-05-25 07:00:53 +0000

    0

--- application.rb
+++ application.rb
@@ -1,9 +1,3 @@
-%w(helpers models controllers).each do |dir|
-  Dir.glob(File.expand_path("../#{dir}", __FILE__) + '/*.rb').each do |file|
-    require file
-  end
-end
-
 libdir = File.expand_path('../../lib', __FILE__)
 a = File.expand_path('./controllers', __FILE__)
 $LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
@@ -14,4 +8,10 @@ configure :production do
   require 'newrelic_rpm'
   enable :logging
   GC::Profiler.enable
+end
+
+%w(helpers models controllers).each do |dir|
+  Dir.glob(File.expand_path("../#{dir}", __FILE__) + '/*.rb').each do |file|
+    require file
+  end
 end
