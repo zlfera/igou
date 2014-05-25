c0df4fea44fd0940f6a432aeef5417d261959872
曾令发 <zlfera@gmail.com>
2014-05-25 07:07:48 +0000

    0

--- application.rb
+++ application.rb
@@ -1,4 +1,6 @@
 libdir = File.expand_path('../../lib', __FILE__)
+a = File.expand_path('./controllers', __FILE__)
+$LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 Mongoid.load!('./config/mongoid.yml', ENV['RACK_ENV'])
 configure :production do
@@ -8,8 +10,6 @@ configure :production do
 end
 
 %w(helpers models controllers).each do |dir|
-  a = File.expand_path("./#{dir}", __FILE__)
-  $LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
   Dir.glob(File.expand_path("../#{dir}", __FILE__) + '/*.rb').each do |file|
     require file
   end
