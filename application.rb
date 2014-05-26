737b2d30d8b7b80b5974beed5fe282955d32e30f
曾令发 <zlfera@gmail.com>
2014-05-26 12:52:27 +0000

    0

--- application.rb
+++ application.rb
@@ -1,6 +1,6 @@
-libdir = File.expand_path('../../lib', __FILE__)
-a = File.expand_path('./controllers', __FILE__)
-$LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
+libdir = File.expand_path('../lib', __FILE__)
+# a = File.expand_path('./controllers', __FILE__)
+# $LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 Mongoid.load!('./config/mongoid.yml', ENV['RACK_ENV'])
 configure :production do
@@ -9,6 +9,15 @@ configure :production do
   GC::Profiler.enable
 end
 
+class Sinatra::Base
+  configure do |c|
+#    c.set :root, File.expand_path("../..", __FILE__)
+    c.set :public_folder, File.expand_path("../public", __FILE__)
+    c.set :views, File.expand_path("../views", __FILE__)
+    c.set :slim, layout_options: { views: 'views/layouts' }
+    c.enable :logging, :static, :sessions
+  end
+end
 %w(helpers models controllers).each do |dir|
   Dir.glob(File.expand_path("../#{dir}", __FILE__) + '/*').each do |file|
     require file
--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,9 +1,9 @@
 #
 module Zlf
 class ApplicationController < Sinatra::Base
-  set :public_folder, File.expand_path('../../public', __FILE__)
-  set :views, File.expand_path('../../views', __FILE__)
-  set :slim, layout_options: { views: 'views/layouts' }
+#  set :public_folder, File.expand_path('../../public', __FILE__)
+#  set :views, File.expand_path('../../views', __FILE__)
+#  set :slim, layout_options: { views: 'views/layouts' }
   # set :static, false
   helpers ApplicationHelper
 
