5c139d7edee3e2524262da9e6bdf7f5593f2b29f
曾令发 <zlfera@gmail.com>
2014-05-28 08:59:00 +0000

    0

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,5 +1,5 @@
 #
-class ApplicationController < Sinatra::Base
+class ApplicationController < Sinatra::Application
 #  set :public_folder, File.expand_path('../../public', __FILE__)
 #  set :views, File.expand_path('../../views', __FILE__)
 #  set :slim, layout_options: { views: 'views/layouts' }
