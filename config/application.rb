5908224ad54903facc3786fb3598d832d65b7169
曾令发 <zlfera@gmail.com>
2014-05-28 06:58:33 +0000

    0

--- config/application.rb
+++ config/application.rb
@@ -3,12 +3,14 @@
     load file
   end
 end
+Bundler.require(:default, ENV['RACK_ENV'])
 
 libdir = File.expand_path('../../lib', __FILE__)
 # a = File.expand_path('./controllers', __FILE__)
 # $LOAD_PATH.unshift(a) unless $LOAD_PATH.include?(a)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 Mongoid.load!('./config/mongoid.yml', ENV['RACK_ENV'])
+
 configure :production do
   require 'newrelic_rpm'
   enable :logging
