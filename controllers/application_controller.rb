cf68d3eaae0a8afc634d5ebd452423e44f781e11
曾令发 <zlfera@gmail.com>
2014-06-01 05:01:53 +0000

    0

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
     slim :'home/create'
   end
 
-  not_found do
-    'sorry,there is no you want'
-  end
+  #not_found do
+   # 'sorry,there is no you want'
+  #end
 end
