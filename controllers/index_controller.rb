b9c48d63e365af7c5c2639c71e55dde516a2d1e8
曾令发 <zlfera@gmail.com>
2014-06-01 03:25:19 +0000

    0

--- controllers/index_controller.rb
+++ controllers/index_controller.rb
@@ -3,6 +3,7 @@ class IndexController < ApplicationController
   get '/index' do
     @css_path = '/css/index.css'
     @title_name = 'zeng wang zhi dao hang'
-    slim :'zeng/index'
+    'hello'
+    #slim :'zeng/index'
   end
 end
